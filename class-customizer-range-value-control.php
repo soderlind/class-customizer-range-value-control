63ec560d6b906ac6aa478c780db596244553dd8b
soderlind <per@soderlind.no>
2016-12-25 14:20:36 +0100

    Add code

---
+++ class-customizer-range-value-control.php
@@ -0,0 +1,35 @@
+<?php
+class Customizer_Range_Value_Control extends WP_Customize_Control {
+	public $type = 'range-value';
+
+	/**
+	 * Enqueue scripts/styles.
+	 *
+	 * @since 3.4.0
+	 */
+	public function enqueue() {
+		wp_enqueue_script( 'customizer-range-value-control', get_stylesheet_directory_uri() . '/js/customizer-range-value-control.js', array( 'jquery' ), rand(), true );
+		wp_enqueue_style( 'customizer-range-value-control', get_stylesheet_directory_uri() . '/css/customizer-range-value-control.css', array(), rand() );
+	}
+
+	/**
+	 * Render the control's content.
+	 *
+	 * @author soderlind
+	 * @version 1.2.0
+	 */
+	public function render_content() {
+		?>
+		<label>
+			<span class="customize-control-title"><?php echo esc_html( $this->label ); ?></span>
+			<div class="range-slider"  style="width:100%; display:flex;flex-direction: row;justify-content: flex-start;">
+				<span  style="width:100%; flex: 1 0 0; vertical-align: middle;"><input class="range-slider__range" type="range" value="<?php echo esc_attr( $this->value() ); ?>" <?php $this->input_attrs(); $this->link(); ?>>
+				<span class="range-slider__value">0</span></span>
+			</div>
+			<?php if ( ! empty( $this->description ) ) : ?>
+			<span class="description customize-control-description"><?php echo $this->description; ?></span>
+			<?php endif; ?>
+		</label>
+		<?php
+	}
+}
