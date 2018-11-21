376322f56c90004fab9de9a31a3f00613f788d28
soderlind <per@soderlind.no>
2018-11-21 23:38:14 +0100

    Use plugin / theme agnostic path to url convert method

--- class-customizer-range-value-control.php
+++ class-customizer-range-value-control.php
@@ -1,5 +1,5 @@
 <?php
-class Customizer_Range_Value_Control extends WP_Customize_Control {
+class Customizer_Range_Value_Control extends \WP_Customize_Control {
 	public $type = 'range-value';
 
 	/**
@@ -8,8 +8,8 @@ class Customizer_Range_Value_Control extends WP_Customize_Control {
 	 * @since 3.4.0
 	 */
 	public function enqueue() {
-		wp_enqueue_script( 'customizer-range-value-control', get_stylesheet_directory_uri() . '/js/customizer-range-value-control.js', array( 'jquery' ), rand(), true );
-		wp_enqueue_style( 'customizer-range-value-control', get_stylesheet_directory_uri() . '/css/customizer-range-value-control.css', array(), rand() );
+		wp_enqueue_script( 'customizer-range-value-control', $this->abs_path_to_url( dirname( __FILE__ ) . '/js/customizer-range-value-control.js' ), array( 'jquery' ), rand(), true );
+		wp_enqueue_style( 'customizer-range-value-control', $this->abs_path_to_url( dirname( __FILE__ ) . '/css/customizer-range-value-control.css' ), array(), rand() );
 	}
 
 	/**
@@ -23,7 +23,12 @@ class Customizer_Range_Value_Control extends WP_Customize_Control {
 		<label>
 			<span class="customize-control-title"><?php echo esc_html( $this->label ); ?></span>
 			<div class="range-slider"  style="width:100%; display:flex;flex-direction: row;justify-content: flex-start;">
-				<span  style="width:100%; flex: 1 0 0; vertical-align: middle;"><input class="range-slider__range" type="range" value="<?php echo esc_attr( $this->value() ); ?>" <?php $this->input_attrs(); $this->link(); ?>>
+				<span  style="width:100%; flex: 1 0 0; vertical-align: middle;"><input class="range-slider__range" type="range" value="<?php echo esc_attr( $this->value() ); ?>"
+																																				  <?php
+																																					$this->input_attrs();
+																																					$this->link();
+																																					?>
+				>
 				<span class="range-slider__value">0</span></span>
 			</div>
 			<?php if ( ! empty( $this->description ) ) : ?>
@@ -31,5 +36,21 @@ class Customizer_Range_Value_Control extends WP_Customize_Control {
 			<?php endif; ?>
 		</label>
 		<?php
+	}
+
+	/**
+	 * Plugin / theme agnostic path to URL
+	 *
+	 * @see https://wordpress.stackexchange.com/a/264870/14546
+	 * @param string $path  file path
+	 * @return string       URL
+	 */
+	private function abs_path_to_url( $path = '' ) {
+		$url = str_replace(
+			wp_normalize_path( untrailingslashit( ABSPATH ) ),
+			site_url(),
+			wp_normalize_path( $path )
+		);
+		return esc_url_raw( $url );
 	}
 }
